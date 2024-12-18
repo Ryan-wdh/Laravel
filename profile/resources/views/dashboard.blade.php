6ae6da48777b01908de0c91803c8bfbb1bcbe832
Ryan <s1216264@student.windesheim.nl>
2024-12-18 17:52:45 +0100

    darkmode added, search bar added, festival name added with joins (fix connections

--- profile/resources/views/dashboard.blade.php
+++ profile/resources/views/dashboard.blade.php
@@ -1,20 +1,20 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
-                <div class="p-6 text-gray-900">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
                     <p>Here are your currently booked trips:</p>
                     @foreach ($buses as $bus)
-                        <div class="w-auto bg-amber-50 p-4 text-center border border-dashed border-gray-900 rounded mb-4">
+                        <div class="w-auto bg-gray-500 p-4 text-center border border-dashed border-gray-900 rounded mb-4">
                             <p>Festival titel: {{ $bus->title }}</p>
                             <p>Bus Vertrektijd: {{ $bus->leaves_at }}</p>
                             <p>Prijs: €{{ $bus->ticket_price }}</p>
--- profile/resources/views/festivals/show.blade.php
+++ profile/resources/views/festivals/show.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <h1 class="mt-5 text-3xl text-center">Busreizen</h1>
+    <h1 class="mt-5 text-3xl text-center text-gray-100">Busreizen</h1>
     @if (session('success'))
         <div class="w-1/4 mx-auto bg-green-200 text-green-800 p-3 rounded mb-4">
             {{ session('success') }}
@@ -7,7 +7,7 @@
     @endif
     <div class="mt-5 mx-auto w-3/4 grid grid-cols-3 gap-5">
             @foreach ($buses as $bus)
-                    <div class="w-auto bg-amber-50 p-4 text-center border border-dashed border-gray-900 rounded">
+                    <div class="w-auto bg-gray-600 text-gray-100 p-4 text-center border border-dashed border-gray-900 rounded">
                         <h2 class="font-bold">Festival Naam: {{$festival?->title ?? 'Geen festival gekoppeld' }}</h2>
                         <p>Bus Vertrektijd: {{ $bus->leaves_at }}</p>
                         <p>Prijs: €{{$bus->ticket_price}}</p>
